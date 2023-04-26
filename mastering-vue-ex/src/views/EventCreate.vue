d363fdc11f63772ef8860e215ca4672a480eac2f
Stephen Aranda <aranda.stephen88@gmail.com>
2023-04-26 10:20:02 -0500

    added option element for categories

--- mastering-vue-ex/src/views/EventCreate.vue
+++ mastering-vue-ex/src/views/EventCreate.vue
@@ -5,8 +5,7 @@
     <form>
        <label>Select a Category</label>
        <select v-model="event.category">
-           
-
+          <option v-for="cat in categories" :key="cat">{{ cat }}</option> 
        </select>
 
     </form>
