a32b69beac10d921e9787de1c04b4f46dcddec7b
Stephen Aranda <aranda.stephen88@gmail.com>
2023-04-26 10:21:52 -0500

    added a div for field class

--- mastering-vue-ex/src/views/EventCreate.vue
+++ mastering-vue-ex/src/views/EventCreate.vue
@@ -8,6 +8,9 @@
           <option v-for="cat in categories" :key="cat">{{ cat }}</option> 
        </select>
        <h3>Name & describe your event</h3>
+       <div class="field">
+        
+       </div>
 
     </form>
 
