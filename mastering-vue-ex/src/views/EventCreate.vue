946af172cbb667bbd2877c968c0b324eacfbc9e9
Stephen Aranda <aranda.stephen88@gmail.com>
2023-04-27 08:30:59 -0500

    added sec title

--- mastering-vue-ex/src/views/EventCreate.vue
+++ mastering-vue-ex/src/views/EventCreate.vue
@@ -16,12 +16,13 @@
           <label>Description</label>
           <input v-model="event.description" type="text" placeholder="Add a description"/>
        </div>
-       <h3>Where is your Event?</h3>
+       <h3>Where is your event?</h3>
        <div class="field">
           <label>Location</label>
           <input v-model="event.location" type="text" placeholder="Add a location"/>
-
        </div>
+       <h3>When is your event?</h3>
+
          
 
     </form>
